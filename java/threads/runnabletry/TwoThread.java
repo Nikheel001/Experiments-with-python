7096ff9edde0bdd3207e4a1ad2aaf45da1baf4ac
Nikheel001 <nikheel.patelce@gmail.com>
2022-03-05 08:57:59 +0530

    moved runnable related files to runnabletry

--- java/threads/runnabletry/TwoThread.java
+++ java/threads/runnabletry/TwoThread.java
@@ -1,14 +1,38 @@
+package runnabletry;
+
+import java.util.Date;
+import java.util.logging.ConsoleHandler;
+import java.util.logging.Formatter;
+import java.util.logging.Handler;
+import java.util.logging.LogRecord;
+import java.util.logging.Logger;
+
 /**
  * TwoThread
  */
 public class TwoThread {
 
+
+    static Logger log = Logger.getLogger(ManyThreadOneTask.class.getName());
+    static Handler h = new ConsoleHandler();
+    static Formatter f = new Formatter() {
+        @Override
+        public String format(LogRecord record) {
+            return String.format(" %s : %s : %s %s",
+                    Thread.currentThread().getName(), record.getMessage(),
+                    new Date(record.getMillis()), System.lineSeparator());
+        }
+    };
+
     public static void main(String[] args) {
+        log.setUseParentHandlers(false);
+        h.setFormatter(f);
+        log.addHandler(h);
 
         // 1. main thread
-        System.out.println("executed by thread :" + Thread.currentThread().getName());
+        log.info("creating OneTask in main");
 
-        OneTask task1 = new OneTask();
+        OneTask task1 = new OneTask(log);
 
         // 2. thread-1
         // three ways to spawn new thread
@@ -42,7 +66,7 @@ public class TwoThread {
         try {
             t.join();
         } catch (InterruptedException e) {
-            System.out.println("thread Interrupted before it could finish execution");
+            log.severe("thread Interrupted before it could finish execution");
         }
     }
 }
